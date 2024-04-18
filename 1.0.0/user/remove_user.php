2874a98b209ed4c9429746bbb2cb86f7a43a85e1
dev-dmitrii-g <gagarindmitrii56@gmail.com>
2024-04-18 12:53:08 +0500

    Made tweaks to a remove_user.php

--- 1.0.0/user/remove_user.php
+++ 1.0.0/user/remove_user.php
@@ -6,17 +6,17 @@ function remove_user(){
   global $db;
 
   // Берем параметр
-  if (isset($_POST["iin"])) {
-    error("Введены неправильные параметры", 400);
+  if (!isset($_POST["iin"])) {
+    error("WRONG DATA", 400);
   }
 
   $iin = $_POST["iin"];
 
-  $db->query("UPDATE `Users` SET `user_role_id` = '0' WHERE `user_iin` = '$iin'");
+  $db->query("UPDATE `Users` SET `user_role_id` = '0', `user_password` = NULL WHERE `user_iin` = '$iin'");
 
   if ($db->affected_rows > 0) {
-    error("Пользователь успешно ограничен в доступе", 200);
+    error("OK", 200);
   } else {
-    error("Произошла ошибка при выполнении запроса", 400);
+    error("BAD REQUEST", 400);
   }
 }
